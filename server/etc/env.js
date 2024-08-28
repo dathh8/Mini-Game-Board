902b1654fd45588c1712508b1d9b077b74d43c07
dathh <dathh@smartosc.com>
2024-08-28 15:20:45 +0700

    Init project

---
+++ server/etc/env.js
@@ -0,0 +1,11 @@
+module.exports = {
+    host : 'localhost',
+    port : 3001,
+    db : {
+        host: 'localhost',
+        user: 'root',
+        password: 'Huudat.2309',
+        database: 'mini_game_board_v1'
+    },
+    JWT_ACCESS_TOKEN_SECRET: "test"
+}
---
+++ server/index.js
@@ -0,0 +1,16 @@
+// create a new express app
+const express = require('express');
+const env = require('./etc/env');
+const app = express();
+const port = env.port;
+// const routers = require('./routers/routers');
+
+// Object.keys(routers.MAPPING_ROUTERS).forEach(function(key, idx, arr){
+//     app.use(key, routers.MAPPING_ROUTERS[key])
+// });
+
+// make the app listen to port 3000
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+    }
+);
